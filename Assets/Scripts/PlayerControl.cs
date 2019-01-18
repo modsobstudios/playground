52191406918a9bd95253d4579a30483c47592890
modsobstudios <modsobgaming@gmail.com>
2019-01-18 06:23:14 -0600

    Set folder structure. Added PlayerControl.cs. Player can move but not look. -MP

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour 
+{
+	public float speed = 10.0f;
+	private Rigidbody rb;
+	private float xMov, zMov;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		xMov = Input.GetAxis ("Horizontal");
+		zMov = Input.GetAxis ("Vertical");
+		doMove ();
+		doRotate ();
+	}
+
+	private void doMove()
+	{
+		if (xMov == 0.0f && zMov == 0.0f)
+			rb.velocity = new Vector3 (rb.velocity.x * 0.5f, rb.velocity.y, rb.velocity.z * 0.5f);
+		rb.AddForce (new Vector3 (xMov, 0.0f, zMov) * speed);
+	}
+
+	private void doRotate()
+	{
+		
+	}
+
+}
