a0724d487a9d3aae11014a256d2396e722abd096
modsobstudios <modsobgaming@gmail.com>
2019-01-18 18:40:49 -0600

    Perfected mouseLook and player movement. Untested with slopes/walls. -MP

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,45 +5,173 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour 
 {
 	private float speed = 10.0f;
-    private float lookSpeed = 5.0f;
 	private Rigidbody rb;
-    private float xRot, zzRot;
 
-	// Use this for initialization
-	void Start () 
+    // blatantlyStolenMouseLookCode
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 5F;
+    public float sensitivityY = 5F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationX = 0F;
+    float rotationY = 0F;
+
+    private List<float> rotArrayX = new List<float>();
+    float rotAverageX = 0F;
+
+    private List<float> rotArrayY = new List<float>();
+    float rotAverageY = 0F;
+
+    public float tickCount = 20;
+
+    public float maximumVelocity = 10;
+
+    Quaternion originalRotation;
+
+    // Use this for initialization
+    void Start () 
 	{
 		rb = GetComponent<Rigidbody> ();
-	}
+
+        // blatantlyStolenMouseLookCode
+        if (rb)
+            rb.freezeRotation = true;
+        originalRotation = transform.localRotation;
+    }
 	
-	// Update is called once per frame
 	void FixedUpdate () 
 	{
 		doMove ();
-		doRotate ();
+        blatantlyStolenMouseLookCode();
 	}
 
+    // Legit probably the best FPS movement code I've done. So simple. So clean. 
 	private void doMove()
 	{
         if (Input.GetKey(KeyCode.A))
-            rb.AddForce(-transform.right * speed);
+            rb.velocity = new Vector3(-transform.right.x * speed, rb.velocity.y, -transform.right.z * speed);
         if (Input.GetKey(KeyCode.D))
-            rb.AddForce(transform.right * speed);
+            rb.velocity = new Vector3(transform.right.x * speed, rb.velocity.y, transform.right.z * speed);
         if (Input.GetKey(KeyCode.W))
-            rb.AddForce(transform.forward * speed);
+            rb.velocity = new Vector3(transform.forward.x * speed, rb.velocity.y, transform.forward.z * speed);
         if (Input.GetKey(KeyCode.S))
-            rb.AddForce(-transform.forward * speed);
+            rb.velocity = new Vector3(-transform.forward.x * speed, rb.velocity.y, -transform.forward.z * speed);
 
-        if(!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
             rb.velocity = new Vector3(rb.velocity.x * 0.5f, rb.velocity.y, rb.velocity.z * 0.5f);
-	}
-
-	private void doRotate()
-	{
-        xRot = Input.GetAxis("Mouse X");
-        zzRot = Input.GetAxis("Mouse Y");
-        // This is currently very bad.
-        transform.Rotate(new Vector3(-zzRot, 0, 0) * lookSpeed);
-        transform.Rotate(new Vector3(0, xRot, 0) * lookSpeed);
     }
 
+    // Don't you worry about this here code.
+    private void blatantlyStolenMouseLookCode()
+    {
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            rotAverageY = 0f;
+            rotAverageX = 0f;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotArrayY.Add(rotationY);
+            rotArrayX.Add(rotationX);
+
+            if (rotArrayY.Count >= tickCount)
+            {
+                rotArrayY.RemoveAt(0);
+            }
+            if (rotArrayX.Count >= tickCount)
+            {
+                rotArrayX.RemoveAt(0);
+            }
+
+            for (int j = 0; j < rotArrayY.Count; j++)
+            {
+                rotAverageY += rotArrayY[j];
+            }
+            for (int i = 0; i < rotArrayX.Count; i++)
+            {
+                rotAverageX += rotArrayX[i];
+            }
+
+            rotAverageY /= rotArrayY.Count;
+            rotAverageX /= rotArrayX.Count;
+
+            rotAverageY = ClampAngle(rotAverageY, minimumY, maximumY);
+            rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
+
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotAverageY, Vector3.left);
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotAverageX, Vector3.up);
+
+            transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            rotAverageX = 0f;
+
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotArrayX.Add(rotationX);
+
+            if (rotArrayX.Count >= tickCount)
+            {
+                rotArrayX.RemoveAt(0);
+            }
+            for (int i = 0; i < rotArrayX.Count; i++)
+            {
+                rotAverageX += rotArrayX[i];
+            }
+            rotAverageX /= rotArrayX.Count;
+
+            rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
+
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotAverageX, Vector3.up);
+            transform.localRotation = originalRotation * xQuaternion;
+        }
+        else
+        {
+            rotAverageY = 0f;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+            rotArrayY.Add(rotationY);
+
+            if (rotArrayY.Count >= tickCount)
+            {
+                rotArrayY.RemoveAt(0);
+            }
+            for (int j = 0; j < rotArrayY.Count; j++)
+            {
+                rotAverageY += rotArrayY[j];
+            }
+            rotAverageY /= rotArrayY.Count;
+
+            rotAverageY = ClampAngle(rotAverageY, minimumY, maximumY);
+
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotAverageY, Vector3.left);
+            transform.localRotation = originalRotation * yQuaternion;
+        }
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        angle = angle % 360;
+        if ((angle >= -360F) && (angle <= 360F))
+        {
+            if (angle < -360F)
+            {
+                angle += 360F;
+            }
+            if (angle > 360F)
+            {
+                angle -= 360F;
+            }
+        }
+        return Mathf.Clamp(angle, min, max);
+    }
 }
